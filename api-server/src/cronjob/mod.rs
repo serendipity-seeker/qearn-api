794f5204daea99973f63388d2171e7f37c41756e
serendipity-seeker <valkyrie.prim@gmail.com>
2025-01-25 04:55:26 +0900

    feat: upsert tickinfo

--- api-server/src/cronjob/mod.rs
+++ api-server/src/cronjob/mod.rs
@@ -1,5 +1,5 @@
 use crate::service::rpc_service::fetch_tick_info;
-use crate::db::queries::create_tick_info;
+use crate::db::queries::upsert_tick_info;
 use crate::db::prisma::PrismaClient;
 use std::error::Error;
 
@@ -8,7 +8,7 @@ pub async fn cronjob(client: &PrismaClient) -> Result<(), Box<dyn Error>> {
     match fetch_tick_info().await {
         Ok(tick_info) => {
             tracing::info!("Tick info: {:?}", tick_info);
-            create_tick_info(&client, tick_info.tick, tick_info.duration, tick_info.epoch, tick_info.initial_tick).await?;
+            upsert_tick_info(&client, tick_info.tick, tick_info.duration, tick_info.epoch, tick_info.initial_tick).await?;
             Ok(())
         }
         Err(err) => {
--- api-server/src/db/queries.rs
+++ api-server/src/db/queries.rs
@@ -23,14 +23,34 @@ pub async fn get_user_by_id(client: &PrismaClient, user_id: i32) -> Result<Optio
     ).exec().await
 }
 
-pub async fn create_tick_info(client: &PrismaClient, tick: i64, duration: i64, epoch: i32, initial_tick: i64) -> Result<prisma::tick_info::Data, QueryError> {
-    client.tick_info().create(
-        tick,
-        duration,
-        epoch,
-        initial_tick,
-        vec![],
-    ).exec().await
+pub async fn upsert_tick_info(client: &PrismaClient, tick: i64, duration: i64, epoch: i32, initial_tick: i64) -> Result<prisma::tick_info::Data, QueryError> {
+    let old_tick_info = client.tick_info().find_first(vec![])
+        .order_by(prisma::tick_info::created_at::order(prisma_client_rust::Direction::Desc))
+        .exec()
+        .await?;
+
+    match old_tick_info {
+        Some(existing_tick) => {
+            client.tick_info().update(
+                prisma::tick_info::id::equals(existing_tick.id),
+                vec![
+                    prisma::tick_info::tick::set(tick),
+                    prisma::tick_info::duration::set(duration),
+                    prisma::tick_info::epoch::set(epoch),
+                    prisma::tick_info::initial_tick::set(initial_tick),
+                ],
+            ).exec().await
+        },
+        None => {
+            client.tick_info().create(
+                tick,
+                duration,
+                epoch,
+                initial_tick,
+                vec![],
+            ).exec().await
+        }
+    }
 }
 
 pub async fn get_tick_info(client: &PrismaClient) -> Result<Vec<prisma::tick_info::Data>, QueryError> {
